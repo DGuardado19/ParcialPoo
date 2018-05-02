6e3135cae98950d91161a36631f40504ba713178
DGuardado19 <00184217@uca.edu.sv>
2018-05-02 14:21:21 -0600

    Creando validacion para el dui del cliente

--- src/parcialpoo/DUI.java
+++ src/parcialpoo/DUI.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package parcialpoo;
+
 import java.util.Scanner;
 
 /**
@@ -11,13 +12,36 @@ import java.util.Scanner;
  * @author DGuardado <david.guardado at guardado.org>
  */
 public class DUI {
-    Scanner d = new Scanner(System.in);
-    private String dui;
-    public DUI(){
-        
+
+    int n;
+    Scanner leer = new Scanner(System.in);
+
+    public DUI() {
     }
-    public void PedirD(){
-        System.out.println("Ingrese el diu del cliente porfavor :) : \n");
-        dui= d.nextLine();
+
+    String[] PedirD(int a) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Ingrese el numero verificador del dui :) :");
+//        int verificador = sc.nextInt();
+//        Scanner a = new Scanner(System.in);
+//        System.out.println("Ingrese los numeros enteros que quiera ingresar separados por una coma ',' , Presione Enter para terminar: ");
+//        String dui =a.nextLine();
+//        String [] du = dui.split(",");
+        int[] arreglo = new int[a];
+
+        for (int i = 0; i < a; i++) {
+            System.out.print("Numero " + String.valueOf(i + 1) + ": ");
+            arreglo[i] = leer.nextInt();
+            
+        }
+
+        System.out.println("");
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("Numero " + String.valueOf(i + 1) + ": " + String.valueOf(arreglo[i]));
+        }
+
+//        return du;
+
     }
 }
