d0e233cfa9cc236c74f19a96956377eaca76cf3d
DGuardado19 <00184217@uca.edu.sv>
2018-05-01 18:55:59 -0600

    Creando la objeto DUI

---
+++ src/parcialpoo/DUI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author DGuardado <david.guardado at guardado.org>
+ */
+public class DUI {
+    public double dui;
+    
+    
+    
+}
