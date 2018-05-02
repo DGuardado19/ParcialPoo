9e2d33d1b64e2a8e69991bd2297ba9f7409b9517
Eduardo López <00092117@uca.edu.sv>
2018-05-02 17:52:01 -0600

    Metodo main

--- src/parcialpoo/main.java
+++ src/parcialpoo/main.java
@@ -5,6 +5,8 @@
  */
 package parcialpoo;
 
+import java.util.*;
+
 /**
  *
  * @author DGuardado <david.guardado at guardado.org>
@@ -14,8 +16,9 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args) {        
+        
     }
     
 }
