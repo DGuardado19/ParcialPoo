14b97fef98f79a0f97f716ad7f8a0ca0d9dbdac7
Eduardo López <00092117@uca.edu.sv>
2018-05-02 09:22:26 -0600

    cambie nombre de la clase main y agregue metodo agregarPiso() en la clase piso

--- src/parcialpoo/Piso.java
+++ src/parcialpoo/Piso.java
@@ -1,6 +1,8 @@
 
 package parcialpoo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Eduardo Alberto López Torres <Carnet: 00092117>
@@ -8,5 +10,33 @@ package parcialpoo;
 public class Piso {
     public Piso(){
         
+    }
+    
+    public ArrayList<Piso> Piso = new ArrayList<Piso>();
+    
+    private int codigo;
+    private String Letra;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getLetra() {
+        return Letra;
+    }
+
+    public void setLetra(String Letra) {
+        this.Letra = Letra;
+    }
+    
+    public void agregarPiso(){
+        Piso piso = new Piso();
+        piso.setLetra("A");
+        piso.setCodigo(1);
+        Piso.add(piso);
     }
 }
