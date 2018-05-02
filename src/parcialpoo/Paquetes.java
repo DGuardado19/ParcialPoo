10b64e8f3525e4db8bdb8ca69954c15e163941c3
RobertoHer <00162317@uca.edu.sv>
2018-05-02 14:52:39 -0600

    Ya se pide el nombre del paquete, cuantos servicios entraran y el nombre de los servicios. xd

--- src/parcialpoo/Paquetes.java
+++ src/parcialpoo/Paquetes.java
@@ -5,10 +5,69 @@
  */
 package parcialpoo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author RobertoHer
  */
 public class Paquetes {
-    
+
+    public String nombre;
+    public ArrayList<String> Servicio;
+
+    public Paquetes(String nombre, ArrayList<String> Servicio) {
+        this.nombre = nombre;
+        this.Servicio = Servicio;
+    }
+
+    public Paquetes() {
+        Servicio = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<String> getServicio() {
+        return Servicio;
+    }
+
+    public void setServicio(ArrayList<String> Servicio) {
+        this.Servicio = Servicio;
+    }
+
+    public void agregar(Paquetes pa, ArrayList<String> pat) {
+        Scanner input = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del paquete: ");
+        pa.setNombre(input.nextLine());
+        int valor;
+        System.out.println("Ingrese cuantos servicios agregara: ");
+        valor = entrada.nextInt();
+        for (int i = 0; i < valor; i++) {
+            pat.add("");
+            System.out.println("Ingrese los servicios del paquete: ");
+            pat.set(i, leer.nextLine());
+        }
+        pa.setServicio(pat);
+        this.nombre = pa.getNombre();
+        this.Servicio=pa.getServicio();
+     
+     } 
+    public void mostrar(){
+        System.out.println("--- MOSTRANDO LA LISTA ---");
+        System.out.println("Nombre del paquete: "+getNombre());
+         for(String a: Servicio){
+             System.out.println("Servicios: "+a.toString());
+         }
+     //pat.set(0, leer);
+
+    }
 }
