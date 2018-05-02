c3c256cbeb14c3d8fa20484b9aa69df3a3b0019b
DGuardado19 <37202984+DGuardado19@users.noreply.github.com>
2018-05-02 16:37:16 -0600

    Validacion del dui del cliente finalizado, con todas sus exception

--- src/parcialpoo/DUI.java
+++ src/parcialpoo/DUI.java
@@ -5,6 +5,9 @@
  */
 package parcialpoo;
 
+import com.sun.istack.internal.logging.Logger;
+import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 /**
@@ -13,31 +16,45 @@ import java.util.Scanner;
  */
 public class DUI {
 
-    int n;
     Scanner leer = new Scanner(System.in);
 
     public DUI() {
     }
 
-    public boolean PedirD(String dui, int a) {
-        dui="111111111";
-        int cont=9;
-        int aux, total =0;
-        for (int i=0;i<dui.length()-1;i++){
-        aux= Integer.parseInt(String.valueOf(dui.charAt(i)))*cont;
-        total+=aux;
-            System.out.println(aux);
-            --cont;
-    }
-        aux= Integer.parseInt(String.valueOf(dui.charAt(8)));
-        int b;
-        b= 10-(total%10);
-        System.out.println(b);
-        if(b==aux){
-            System.out.println("El dui es valido, continue con el proceso  de reserva :)");
+    public boolean PedirD() {
+        String dui;
+        System.out.println("ingresa el dui, con todo y su numero validador, sin guiones porfavor(osea un total de 8 digitos) :):");
+       try {
+        dui = leer.nextLine();
+            int cont = 9;
+            int aux, total = 0;
+            for (int i = 0; i < dui.length() - 1; i++) {
+                aux = Integer.parseInt(String.valueOf(dui.charAt(i))) * cont;
+                total += aux;
+                System.out.println(aux);
+                --cont;
+            }
+            aux = Integer.parseInt(String.valueOf(dui.charAt(8)));
+            int b;
+            b = 10 - (total % 10);
+            System.out.println(b);
+            if (b == aux) {
+                System.out.println("El dui es valido, continue con el proceso  de reserva :)");
+            } else {
+                System.out.println("Ups, el dui no es valido :(, intenta de nuevo");
+                PedirD();
+            }
+        } catch (NumberFormatException e) {
+            //System.out.println("You Can Only Enter Numbers!");
+            System.err.println("Por favor ingrese un número");
+            leer.nextLine();
+            PedirD();
         }
-        else
-            System.out.println("Ups, el dui no es valido :(, intenta de nuevo");
+    catch (Exception r){
+        System.out.println("cantidad insuficiente de numeros");
+        PedirD();
+    }
+
         return true;
     }
 }
