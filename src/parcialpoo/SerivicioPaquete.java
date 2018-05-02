731746c9251b8114beac7bc1073063d11f392c77
RobertoHer <00162317@uca.edu.sv>
2018-05-02 12:42:07 -0600

    Vamos ahi mas o menos con la lista del paquete

---
+++ src/parcialpoo/SerivicioPaquete.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author RobertoHer
+ */
+public class SerivicioPaquete {
+     public String acceso;//A cuantos accesos tendra el nombre del paquete
+
+    public String getAcceso() {
+        return acceso;
+    }
+
+    public void setAcceso(String acceso) {
+        this.acceso = acceso;
+    }
+        
+}
