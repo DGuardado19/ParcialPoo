d055dad19e5080602482316e8c6762d621659c9f
RobertoHer <00162317@uca.edu.sv>
2018-05-02 20:51:35 -0600

    Se le pide al usuario el dia, mes, anio y lo guarda en un ArrayList

--- src/parcialpoo/Fecha.java
+++ src/parcialpoo/Fecha.java
@@ -4,11 +4,69 @@
  * and open the template in the editor.
  */
 package parcialpoo;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 /**
  *
  * @author RobertoHer
  */
 public class Fecha {
-    
-}
+    private int dia;
+    private int mes;
+    private int anio;
+    private ArrayList<Fecha> datos;
+    public Fecha(){
+        datos = new ArrayList<>();
+    }
+
+    public Fecha(int dia, int mes, int anio){
+        this.dia=dia;
+        this.mes=mes;
+        this.anio=anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    public void add(Fecha dato){
+       datos.add(dato);
+       Scanner input = new Scanner(System.in);
+       
+        System.out.println("Ingrese el dia: ");
+        dato.setDia(input.nextInt());
+        
+        System.out.println("Ingrese el mes: ");
+        dato.setMes(input.nextInt());
+        
+        System.out.println("Ingrese el anio: ");
+        dato.setAnio(input.nextInt());
+        
+        
+    }
+    public void mostrar(){
+        for(Fecha yu : datos){
+            System.out.println("La fecha es: "+yu.getDia()+"/"+yu.getMes()+"/"+yu.getAnio()+"/");
+        }
+    }
+    }
+
