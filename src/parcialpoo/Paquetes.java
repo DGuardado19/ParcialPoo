fe240b235218aaf4ca777301ab899473333031ea
RobertoHer <00162317@uca.edu.sv>
2018-05-03 18:57:20 -0600

    Agregandoooo paquetes de nuevoo jajaja

--- src/parcialpoo/Paquetes.java
+++ src/parcialpoo/Paquetes.java
@@ -7,6 +7,7 @@ package parcialpoo;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -15,7 +16,10 @@ import java.util.Scanner;
 public class Paquetes {
 
     public String nombre;
+    public int precio;
     public ArrayList<String> Servicio;
+    public ArrayList<Paquetes> paque = new ArrayList<>();
+    int cont = 0;
 
     public Paquetes(String nombre, ArrayList<String> Servicio) {
         this.nombre = nombre;
@@ -41,58 +45,91 @@ public class Paquetes {
     public void setServicio(ArrayList<String> Servicio) {
         this.Servicio = Servicio;
     }
+
+    public int getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
     //Agregando paquetes
-    public void agregar(Paquetes pa, ArrayList<String> pat) {
+    public void agregar(String pa, int pre){
+        Paquetes paq = new Paquetes();
+        ArrayList<String> aux = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
-        Scanner leer = new Scanner(System.in);
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese el nombre del paquete: ");
-        pa.setNombre(input.nextLine());
+        //System.out.println("Ingrese el nombre del paquete: ");
+        paq.setNombre(pa);
+        
+        //System.out.println("Ingrese el precio del paquete: ");
+        paq.setPrecio(pre);
         int valor;
         System.out.println("Ingrese cuantos servicios agregara: ");
-        valor = entrada.nextInt();
-        for (int i = 0; i < valor; i++) {
-            pat.add("");
-            System.out.println("Ingrese los servicios del paquete: ");
-            pat.set(i, leer.nextLine());
+        valor = input.nextInt();
+        
+        for(int i = 0;i<valor;i++){
+            System.out.print("Servicio: ");
+            aux.add(input.next());   
+            
         }
-        pa.setServicio(pat);
-        this.nombre = pa.getNombre();
-        this.Servicio=pa.getServicio();
-     
-     } 
-    //Mostrando los datos de los paquetes
+        //System.out.println("Numero del paquete: "+cont);
+        paq.setServicio(aux);
+        paque.add(paq);     
+    }
     public void mostrar(){
-        System.out.println("--- MOSTRANDO LA LISTA ---");
-        System.out.println("Nombre del paquete: "+getNombre());
-         for(String a: Servicio){
-             System.out.println("Servicios: "+a.toString());
-         }
-     //pat.set(0, leer);
+        
+        for(Paquetes correr : paque){
+            //cont++;
+            //System.out.println("Paquete numero: "+cont);
+            System.out.println("Nombre al paquete: "+correr.getNombre());
+            System.out.println("Precio: $"+correr.getPrecio());
+         
+            
+            for(String correr2 : correr.getServicio()){
+                System.out.println("Servicios: "+correr2);
+                
+            }
+        }
+    }
+    
+    //Eliminando todos los paquetes
+    public void clear() {
+        Servicio.clear();
+       paque.clear();
+        System.out.println("--- PAQUETES ELIMINADOS ---");
 
     }
-    //Eliminando todos los paquetes
-    public void clear(){
-        System.out.println("--- PAQUETES ELIMINADOS ---");
-        System.out.println("PAQUETE ELIMINADO: "+getNombre());
-        Servicio.clear();//Por el momento elimina todo PERO sigue mostrando el nombre del paquete
-        
-    }
+
     //Solo para ver cuantos accesos tiene un paquete
-    public void size(){
-        System.out.println("Del paquete: "+getNombre());
+    public void size() {
+        System.out.println("Del paquete: " + getNombre());
         System.out.println("Numero de accesos al paquete: ");
         System.out.println(Servicio.size());
     }
+
     //Para agregar un nuevo acceso a una posicion que solicite el usuario
-    public void add(int numero, String acceso){
-        System.out.println("Del paquete "+getNombre()+" se agrego: ");
-        Servicio.add(numero,acceso);//Por el momento para un paquete.
+    public void add(int numero, String acceso) {
+        ArrayList<String> aux = new ArrayList<String>();
+        Paquetes auxi = new Paquetes();
+        
+        auxi.setNombre(paque.get(numero-1).getNombre());
+        auxi.setPrecio(paque.get(numero-1).getPrecio());
+        
+        aux = paque.get(numero-1).getServicio();
+        aux.add(acceso);        
+        auxi.setServicio(aux);
+        
+        paque.set(numero-1, auxi);        
+        
     }
+
     //Devuelve la posicion del acceso de un paquete
-    public void get(int n){
-        System.out.println("Del paquete: "+getNombre());
+    public void get(int n) {
+        System.out.println("Del paquete: " + getNombre());
         Servicio.get(n);
-        System.out.println("La posicion es: "+Servicio.get(n));
+        System.out.println("La posicion es: " + Servicio.get(n));
     }
+
 }
